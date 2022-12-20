2374b86f60f23758f6f9a4e98e1f02be891c5e25
Miliyonayalew <miliayalew@gmail.com>
2022-12-20 23:05:39 +0300

    Fix rubocop error

--- person.rb
+++ person.rb
@@ -25,5 +25,4 @@ class Person < Nameable
   def of_age?
     @age >= 18
   end
-
 end
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -5,4 +5,3 @@ class TrimmerDecorator < Decorator
     @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
-
